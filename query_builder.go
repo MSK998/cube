ea4a1c9eb58d608d5e0ca304be0b5ec700489f75
Mark Scott-Kiddie <mwscottkiddie@gmail.com>
2023-04-14 14:37:01 +0100

    Initial Commit

---
+++ query_builder.go
@@ -0,0 +1,132 @@
+package cube
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type QueryBuilder struct {
+	table   string
+	selects []string
+	wheres  []string
+	args    []interface{}
+}
+
+func NewQueryBuilder() *QueryBuilder {
+	return &QueryBuilder{}
+}
+
+func (qb *QueryBuilder) Select(selects ...string) *QueryBuilder {
+	qb.selects = append(qb.selects, selects...)
+	return qb
+}
+
+func (qb *QueryBuilder) From(table string) *QueryBuilder {
+	qb.table = table
+	return qb
+}
+
+// Where clause in the form of `...Where("x >= ?", y)`
+func (qb *QueryBuilder) Where(query string, args ...interface{}) *QueryBuilder {
+	qb.wheres = append(qb.wheres, query)
+	qb.args = append(qb.args, args...)
+	return qb
+}
+
+// Select columns from a table based on the interface that is passed into it
+func (qb *QueryBuilder) SelectStruct(obj interface{}) *QueryBuilder {
+	t := reflect.TypeOf(obj)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	if t.Kind() == reflect.Slice {
+		t = t.Elem()
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		qb.Select(field.Name)
+	}
+
+	return qb
+}
+
+func (qb *QueryBuilder) Query(db *sql.DB) (*sql.Rows, error) {
+	query := fmt.Sprintf("SELECT %s FROM %s", strings.Join(qb.selects, ","), qb.table)
+	if len(qb.wheres) > 0 {
+		query += " WHERE " + strings.Join(qb.wheres, " AND ")
+	}
+	return db.Query(query, qb.args...)
+}
+
+// Scan the sql.Rows into the passed struct
+// Some known limitations: 
+// Column names need to match the struct properties exactly or it will zero the value
+func ScanStruct(rows *sql.Rows, out interface{}) error {
+	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	values := make([]interface{}, len(columns))
+	for i := range values {
+		var field interface{}
+		values[i] = &field
+	}
+
+	var reflectValue reflect.Value
+
+	// Check if out is a slice
+	outValue := reflect.ValueOf(out)
+	if outValue.Kind() == reflect.Ptr && outValue.Elem().Kind() == reflect.Slice {
+		reflectValue = outValue.Elem()
+	} else {
+		return errors.New("out must be a pointer to a slice")
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+
+		structPtr := reflect.New(reflectValue.Type().Elem())
+		structVal := structPtr.Elem()
+
+		for i, column := range columns {
+			field := structVal.FieldByName(column)
+			if !field.IsValid() {
+                continue
+			}
+
+			value := reflect.ValueOf(values[i]).Elem().Interface()
+			fieldType := field.Type()
+
+			if fieldType.Kind() == reflect.Slice {
+				sliceType := fieldType.Elem()
+				slice := reflect.MakeSlice(fieldType, 1, 1)
+
+				if sliceType.Kind() == reflect.Uint8 {
+					slice = reflect.ValueOf(value)
+				}
+
+				field.Set(slice)
+			} else {
+				field.Set(reflect.ValueOf(value).Convert(fieldType))
+			}
+		}
+		reflectValue = reflect.Append(reflectValue, structVal)
+	}
+
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	outValue.Elem().Set(reflectValue)
+
+	return nil
+}
---
+++ query_builder_test.go
@@ -0,0 +1,74 @@
+package cube
+import (
+	"database/sql"
+	"testing"
+
+	_ "modernc.org/sqlite"
+)
+
+func TestQueryBuilder(t *testing.T) {
+	// Open a test database
+	db, err := sql.Open("sqlite", ":memory:")
+	if err != nil {
+		t.Fatalf("Failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	// Create a test table
+	_, err = db.Exec(`
+		CREATE TABLE users (
+			ID INTEGER PRIMARY KEY,
+			Name TEXT NOT NULL,
+			Email TEXT NOT NULL UNIQUE,
+			Age INTEGER NOT NULL
+		)
+	`)
+	if err != nil {
+		t.Fatalf("Failed to create table: %v", err)
+	}
+
+	// Insert test data
+	_, err = db.Exec(`
+		INSERT INTO users (name, email, age)
+		VALUES
+			('Alice', 'alice@example.com', 25),
+			('Bob', 'bob@example.com', 30),
+			('Charlie', 'charlie@example.com', 35),
+			('Dave', 'dave@example.com', 40)
+	`)
+	if err != nil {
+		t.Fatalf("Failed to insert data: %v", err)
+	}
+
+	// Test the QueryBuilder
+	var users []struct {
+		ID    int
+		Name  string
+		Email string
+		Age   int
+	}
+	qb := NewQueryBuilder().SelectStruct(&users).From("users").Where("age >= ?", 30)
+	rows, err := qb.Query(db)
+	if err != nil {
+		t.Fatalf("Query failed: %v", err)
+	}
+	err = ScanStruct(rows, &users)
+	if err != nil {
+		t.Fatalf("Scan failed: %v", err)
+	}
+	if len(users) != 3 {
+		t.Fatalf("Expected 3 users, got %d", len(users))
+	}
+	if users[0].ID != 2 || users[1].ID != 3 || users[2].ID != 4 {
+		t.Fatalf("Unexpected user IDs: %v", users)
+	}
+	if users[0].Name != "Bob" || users[1].Name != "Charlie" || users[2].Name != "Dave" {
+		t.Fatalf("Unexpected user names: %v", users)
+	}
+	if users[0].Email != "bob@example.com" || users[1].Email != "charlie@example.com" || users[2].Email != "dave@example.com" {
+		t.Fatalf("Unexpected user emails: %v", users)
+	}
+	if users[0].Age != 30 || users[1].Age != 35 || users[2].Age != 40 {
+		t.Fatalf("Unexpected user ages: %v", users)
+	}
+}
