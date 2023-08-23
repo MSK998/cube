85ea65fd39acdb77ca974e89d1c261b731e485a1
Mark Scott-Kiddie <mwscottkiddie@gmail.com>
2023-08-23 01:10:27 +0100

    TEST: Add test

--- query_builder_test.go
+++ query_builder_test.go
@@ -2,16 +2,20 @@ package cube
 
 import (
 	"database/sql"
+	"log"
+	"os"
 	"testing"
 
 	_ "modernc.org/sqlite"
 )
 
-func TestQueryBuilder(t *testing.T) {
-	// Open a test database
-	db, err := sql.Open("sqlite", ":memory:")
+var db *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	db, err = sql.Open("sqlite", ":memory:")
 	if err != nil {
-		t.Fatalf("Failed to open database: %v", err)
+		log.Fatalf("Failed to open database: %v", err)
 	}
 	defer db.Close()
 
@@ -26,7 +30,7 @@ func TestQueryBuilder(t *testing.T) {
 		)
 	`)
 	if err != nil {
-		t.Fatalf("Failed to create table: %v", err)
+		log.Fatalf("Failed to create table: %v", err)
 	}
 
 	// Insert test data
@@ -40,9 +44,13 @@ func TestQueryBuilder(t *testing.T) {
 			('Rob', 'Rob@example.com', 55, 123)
 	`)
 	if err != nil {
-		t.Fatalf("Failed to insert data: %v", err)
+		log.Fatalf("Failed to insert data: %v", err)
 	}
 
+	os.Exit(m.Run())
+}
+
+func TestQueryBuilder(t *testing.T) {
 	// Test the QueryBuilder
 	var users []struct {
 		ID    int
@@ -67,4 +75,31 @@ func TestQueryBuilder(t *testing.T) {
 	if len(users) != 3 {
 		t.Fatalf("Expected 3 users, got %d", len(users))
 	}
+}
+
+func TestOrderBy(t *testing.T) {
+		// Test the QueryBuilder
+		var users []struct {
+			ID    int
+			Name  string
+			Email string
+			Age   int
+			Type  int
+		}
+		qb := NewQueryBuilder().SelectStruct(&users).From("users").Where("age >= ?", 35).OrderBy(false, "age")
+		t.Log(qb.GetStatement())
+		rows, err := qb.Query(db)
+		if err != nil {
+			if err == sql.ErrNoRows{
+				t.Log("No Rows")
+			}
+			t.Fatalf("Query failed: %v", err)
+		}
+		err = ScanStruct(rows, &users)
+		if err != nil {
+			t.Fatalf("Scan failed: %v", err)
+		}
+		if len(users) != 3 {
+			t.Fatalf("Expected 3 users, got %d", len(users))
+		}
 }
